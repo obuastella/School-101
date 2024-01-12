a79cffd4efc8063ec41707caf22745969303d297
Obua Stella <stellaobua123@gmail.com>
2024-01-12 22:44:07 +0100

    WIP: Profile in signup

--- front-end/src/pages/SignUp.jsx
+++ front-end/src/pages/SignUp.jsx
@@ -3,8 +3,14 @@ function SignUp() {
     <section className="w-full h-screen bg-bgColor flex items-center justify-center">
       <div className="border-2 border-red-600 w-1/2 h-5/6 text-center">
         <h1 className="text-center text-white text-3xl mb-6">Sign Up </h1>
-        <h2 className="text-3xl font-semibold text-green-200">Who are you?</h2>
-        <div></div>
+        <h2 className="text-3xl font-semibold text-green-200 mb-6">
+          Who are you?
+        </h2>
+        <div className="flex justify-evenly">
+          <div className="w-24 h-24 rounded-full border-2 "></div>
+          <div className="w-24 h-24 rounded-full border-2 "></div>
+          <div className="w-24 h-24 rounded-full border-2 "></div>
+        </div>
       </div>
     </section>
   );
