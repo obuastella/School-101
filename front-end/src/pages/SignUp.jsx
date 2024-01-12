0d62844fcc12f5115dfc17a96b912d927ab612e3
Obua Stella <stellaobua123@gmail.com>
2024-01-12 22:30:15 +0100

    WIP: SIGN-UP

--- front-end/src/pages/SignUp.jsx
+++ front-end/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 function SignUp() {
   return (
-    <section className="w-full h-screen bg-bgColor">
-      <div className="border-2 border-red-600 w-1/2 h-5/6 m-auto">
+    <section className="w-full h-screen bg-bgColor flex items-center justify-center">
+      <div className="border-2 border-red-600 w-1/2 h-5/6 ">
         <h1 className="text-center text-white">Sign Up Page</h1>
         <h2>Who are you?</h2>
       </div>
